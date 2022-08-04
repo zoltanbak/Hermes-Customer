e444ab6f7c10516613f614593fbf5e37a15d914a
Zoltan Bak <zoltanbak1993@gmail.com>
2022-08-04 17:54:27 +0200

    Added find by id functionality

--- src/main/java/com/zb/hermes/customer/controller/CustomerRestController.java
+++ src/main/java/com/zb/hermes/customer/controller/CustomerRestController.java
@@ -2,6 +2,7 @@ package com.zb.hermes.customer.controller;
 
 import com.zb.hermes.customer.model.Customer;
 import com.zb.hermes.customer.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -9,15 +10,22 @@ import reactor.core.publisher.Flux;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(value = "api/v1/customers")
+@RequestMapping(value = "api/v1")
+@Slf4j
 public class CustomerRestController {
 
     @Autowired
     CustomerRepository customerRepository;
 
-    @GetMapping
+
+    @GetMapping("customers")
     public Flux<Customer> getAllCustomer() {
         return customerRepository.findAll();
+    }
+
+    @GetMapping("search/findById")
+    public Mono<Customer> findById(@RequestParam UUID id) {
+        return customerRepository.findById(id);
     }
 
     @PostMapping
